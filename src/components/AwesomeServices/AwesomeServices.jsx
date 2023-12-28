6d054eb992bebf85130e49b7d8e5e9c62d237f63
yeasin550 <yeasin51570@gmail.com>
2023-12-28 21:26:11 +0600

    Highlighted Features add and customized

--- src/components/AwesomeServices/AwesomeServices.jsx
+++ src/components/AwesomeServices/AwesomeServices.jsx
@@ -14,24 +14,24 @@ const AwesomeServices = () => {
       <Tabs />
       <div className="max-w-screen-xl mx-auto py-8 px-4 lg:py-16 lg:px-6">
         <div className="text-center mb-10">
-          <h2 className="text-4xl tracking-tight font-bold text-primary-800">
+          <h2 className="text-4xl tracking-tight text-[#8861B3] font-bold text-primary-800">
             Highlighted Features
           </h2>
         </div>
         <div className="flex flex-col md:flex-row">
           {/* can help image */}
-          <div className="mr-0 md:mr-8 mb-6 md:mb-0">
+          <div className="w-1/2 h-full mr-0 md:mr-8 mb-6 md:mb-0">
             <img
-              className="w-1/2 md:w-full mx-auto"
-              src="https://placeholder.pics/svg/400"
+              className=" mx-auto"
+              src="https://img.freepik.com/free-vector/hotel-travel-accommodation-concept-template_98292-3969.jpg?w=740&t=st=1703777030~exp=1703777630~hmac=a5e7118f69a4fb14cca96968e8c68c8ddf3b95438870864b544d3631c2dea48c"
               alt="can_help_banner"
             />
           </div>
           {/* end can help image */}
-          <div className="flex-1 flex flex-col sm:flex-row flex-wrap -mb-4 -mx-2">
+          <div className="w-1/2 flex-1 flex flex-col sm:flex-row flex-wrap -mb-4 -mx-2">
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
               <div className="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                <h3 className="text-2xl font-bold text-md mb-6">
+                <h3 className="text-xl font-bold text-md mb-6">
                   Dynamic Personalization:
                 </h3>
                 <p className="text-sm">
@@ -43,7 +43,7 @@ const AwesomeServices = () => {
             </div>
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
               <div className="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                <h3 className="text-2xl font-bold text-md mb-6">
+                <h3 className="text-xl font-bold text-md mb-6">
                   Mobile-Optimized Interface
                 </h3>
                 <p className="text-sm">
@@ -56,7 +56,7 @@ const AwesomeServices = () => {
             </div>
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
               <div className="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                <h3 className="text-2xl font-bold text-md mb-6">
+                <h3 className="text-xl font-bold text-md mb-6">
                   24/7 Customer Support
                 </h3>
                 <p className="text-sm">
@@ -69,12 +69,12 @@ const AwesomeServices = () => {
             </div>
             <div className="w-full sm:w-1/2 mb-4 px-2 ">
               <div className="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                <h3 className="text-2xl font-bold text-md mb-6">
+                <h3 className="text-xl font-bold text-md mb-6">
                   Secure Payment Processing
                 </h3>
                 <p className="text-sm">
-                  We use cutting-edge security measures to protect our
-                  customers sensitive information and ensure the safety of all
+                  We use cutting-edge security measures to protect our customers
+                  sensitive information and ensure the safety of all
                   transactions made on our site.
                 </p>
               </div>
